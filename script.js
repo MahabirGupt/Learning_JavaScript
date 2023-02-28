c50d4cc631f5e00e9010017fa30f87162d62dc5e
Mahabir <mahabirg81@gmail.com>
2023-02-28 11:41:46 +0800

    Uncommented the code

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 <script>
   console.log("Hello there");
   {/* declaring a string text type */}
-  {/* var name = 'Anish'; console.log(name); */}
+  var name = 'Anish'; console.log(name);
   {/* declaring a boolean text type */}
-  {/* var boolean = true; console.log(boolean); var float = 10.6; console.log(float)
+  var boolean = true; console.log(boolean); var float = 10.6; console.log(float)
   console.log(typeof float) var array = [1,2,3]; console.log(array); var array1
   = [1,2,3,'4']; console.log(array1); console.log(array1[2]); var dataValue =
   null; console.log(dataValue); console.log(typeof dataValue); var dataValue1 =
   undefined; console.log(dataValue1); console.log(typeof dataValue1);
   console.log(dataValue == dataValue1); console.log(dataValue === dataValue1);
-  var dataValue2 = NaN; console.log(NaN);  */}
+  var dataValue2 = NaN; console.log(NaN); 
 
 var dataType = {
     child1: "Max"
