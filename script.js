d9c2b907479b6956a3bf0cfb255e6f7abcdae4f8
Mahabir <mahabirg81@gmail.com>
2023-02-28 11:40:38 +0800

    learnt the different types of data

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 <script>
-  console.log("Hello there"); var name = 'Anish'; console.log(name);
+  console.log("Hello there");
+  {/* declaring a string text type */}
+  {/* var name = 'Anish'; console.log(name); */}
+  {/* declaring a boolean text type */}
+  {/* var boolean = true; console.log(boolean); var float = 10.6; console.log(float)
+  console.log(typeof float) var array = [1,2,3]; console.log(array); var array1
+  = [1,2,3,'4']; console.log(array1); console.log(array1[2]); var dataValue =
+  null; console.log(dataValue); console.log(typeof dataValue); var dataValue1 =
+  undefined; console.log(dataValue1); console.log(typeof dataValue1);
+  console.log(dataValue == dataValue1); console.log(dataValue === dataValue1);
+  var dataValue2 = NaN; console.log(NaN);  */}
+
+var dataType = {
+    child1: "Max"
+};
+console.log(dataType);
+console.log(dataType.child1);
+console.log(typeof dataType);
+
 </script>;
